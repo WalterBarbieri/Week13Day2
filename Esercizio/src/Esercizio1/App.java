17ef2f585a2d2e88bdfc51c540e07ce085538bd7
Walter Barbieri <walter90@hotmail.it>
2023-06-27 19:13:20 +0200

    Commit finale - esercizio 1 completamente revisionato, avevo letto male la consegna

--- Esercizio/src/Esercizio1/App.java
+++ Esercizio/src/Esercizio1/App.java
@@ -3,21 +3,15 @@ package Esercizio1;
 public class App {
 
 	public static void main(String[] args) {
+
+		Rettangolo ret = new Rettangolo();
 		Rettangolo ret1 = new Rettangolo(10, 20);
-		Rettangolo ret2 =  new Rettangolo(15, 30);
-		
-		stampaPerimetro(ret1);
-		stampaPerimetro(ret2);
-		stampaArea(ret1);
-		stampaArea(ret2);
- 
-	}
-	
-	private static void stampaPerimetro(Rettangolo ret) {
-		System.out.println("Perimetro: " + ret.perimetro());
-	}
-	private static void stampaArea(Rettangolo ret) {
-		System.out.println("Area: " +ret.area());
+		Rettangolo ret2 = new Rettangolo(15, 30);
+
+		ret.stampaRettangolo(ret1);
+		ret.stampaRettangolo(ret2);
+		ret.stampaDueRettangoli(ret1, ret2);
+
 	}
 
 }
--- Esercizio/src/Esercizio1/Rettangolo.java
+++ Esercizio/src/Esercizio1/Rettangolo.java
@@ -1,23 +1,42 @@
 package Esercizio1;
 
 public class Rettangolo {
-	
-	//Proprietà
+
+	// Proprietà
 	double altezza;
 	double larghezza;
-	
-	//Costruttore
+
+	// Costruttore
 	public Rettangolo(double altezza, double larghezza) {
 		this.altezza = altezza;
 		this.larghezza = larghezza;
 	}
-	
-	//Metodi
-	
-	public double perimetro() {
-		return (this.altezza + this.larghezza) *2;
+
+	public Rettangolo() {
+	};
+
+	// Metodi
+
+	public double perimetro(Rettangolo ret) {
+		return (ret.altezza + ret.larghezza) * 2;
 	}
-	public double area() {
-		return this.altezza * this.larghezza;
+
+	public double area(Rettangolo ret) {
+		return ret.altezza * ret.larghezza;
+	}
+
+	public void stampaRettangolo(Rettangolo ret) {
+		System.out.println("Perimetro: " + perimetro(ret));
+		System.out.println("Area: " + area(ret));
+
+	}
+
+	public void stampaDueRettangoli(Rettangolo ret1, Rettangolo ret2) {
+		System.out.println("Perimetro rettangolo 1: " + perimetro(ret1));
+		System.out.println("Area rettangolo 1: " + area(ret1));
+		System.out.println("Perimetro rettangolo 2: " + perimetro(ret2));
+		System.out.println("Area rettangolo 2: " + area(ret2));
+		System.out.println("Perimetro totale: " + (perimetro(ret1) + perimetro(ret2)));
+		System.out.println("Area totale: " + (area(ret1) + area(ret2)));
 	}
 }
