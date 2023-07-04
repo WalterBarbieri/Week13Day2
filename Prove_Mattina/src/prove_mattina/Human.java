4bd3b1ed56122cb778b4fdbea5d7befb5fe72bb6
Walter Barbieri <walter90@hotmail.it>
2023-07-04 16:55:32 +0200

    1

--- Prove_Mattina/src/prove_mattina/Human.java
+++ Prove_Mattina/src/prove_mattina/Human.java
@@ -1,39 +1,40 @@
 package prove_mattina;
 
 public class Human {
-	//Lista Proprietà
+	// Lista Proprietà
 	String name;
 	String surname;
 	int age;
-	
-	//Lista Costruttori
+
+	// Lista Costruttori
 	public Human(String name, String surname) {
 		this.name = name;
 		this.surname = surname;
 	}
-	
+
 	public Human(String name) {
 		this.name = name;
 	}
-	
-	//Lista Metodi
+
+	// Lista Metodi
 	void sayHello() {
 		System.out.println("Ciao sono " + this.name + " " + this.surname);
 	}
-	
-	//attributi e metodi static -> possiamo accedere a metodi e attributi static senza creare un oggetto.
-	
+
+	// attributi e metodi static -> possiamo accedere a metodi e attributi static
+	// senza creare un oggetto.
+
 	static void staticMethod() {
 		System.out.println("Ciao sono un metodo statico");
 	}
-	
-	//Sovrascrivere metodi nativi
+
+	// Sovrascrivere metodi nativi
 
 	@Override
 	public boolean equals(Object obj) {
-		
+
 		Human s = (Human) obj;
-		if(this.name == s.name && this.surname == s.surname) {
+		if (this.name == s.name && this.surname == s.surname) {
 			return true;
 		} else {
 			return false;
@@ -44,9 +45,5 @@ public class Human {
 	public String toString() {
 		return "Human [name=" + name + ", surname=" + surname + "]";
 	}
-	
-	
-	
-	
-	
+
 }
